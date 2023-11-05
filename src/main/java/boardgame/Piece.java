58041432b83e3bcaf12b6638d97bdd3df3dcf0c7
Kaio Rodrigo <dossantos.krf@gmail.com>
2023-11-05 18:19:29 -0300

    Possible movies of a piece

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -2,7 +2,7 @@ package boardgame;
 
 //	Classe de posições das peças
 
-public class Piece {
+public abstract class Piece {
 
     protected Position position;
     private Board board;
@@ -17,5 +17,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] matriz = possibleMoves();
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz.length; j++){
+                if(matriz[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
     }
 
     // Método para mover as peças da posição de origem para posição desejada
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
@@ -49,6 +50,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position){
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position.");
+        }
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible moves for the chosen piece.");
         }
     }
 
