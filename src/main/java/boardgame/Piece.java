ffd99ec378c06488add187d79e09974955f332df
Kaio Rodrigo <dossantos.krf@gmail.com>
2023-11-11 17:15:14 -0300

    Implementing possible moves of Rook

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -19,14 +19,14 @@ public abstract class Piece {
         return board;
     }
 
-    public abstract boolean[][] possibleMoves();
+    public abstract boolean[][] possibleMove();
 
     public boolean possibleMove(Position position){
-        return possibleMoves()[position.getRow()][position.getColumn()];
+        return possibleMove()[position.getRow()][position.getColumn()];
     }
 
     public boolean isThereAnyPossibleMove(){
-        boolean[][] matriz = possibleMoves();
+        boolean[][] matriz = possibleMove();
         for(int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz.length; j++){
                 if(matriz[i][j]){
--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -4,6 +4,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -12,6 +13,11 @@ public abstract class ChessPiece extends Piece {
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position){
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return  p != null && p.getColor() != color;
     }
 
     public Color getColor() {
